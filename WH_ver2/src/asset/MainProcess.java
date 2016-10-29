b3d78b653c922ea506c3f2fccf66dc14abc91ec2
oneoftheidiots <antonela.mrkalj@gmail.com>
2016-10-29 15:29:12 +0100

    Update MainProcess.java

--- WH_ver2/src/asset/MainProcess.java
+++ WH_ver2/src/asset/MainProcess.java
@@ -21,16 +21,9 @@ public class MainProcess {
 		
 		Image img = i.preprocessing(red_s, path_s);
 		Image w = i.preprocessing(red_w, path_w);
-		
-		bar.setValue(20);
-		
+	
 		i.inserting(img, w, b, p, a);
-		
-		bar.setValue(75);
-		
 		i.postprocessing(img, save, extension);
-		
-		bar.setValue(95);
 	}
 	
 	static public void startOut(JProgressBar bar, Extract e, BlockChoice b, CoefChoice p, Alpha a,
@@ -40,15 +33,9 @@ public class MainProcess {
 		Image img = e.preprocessing(red_s, path_s);
 		Image w = e.preprocessing(red_w, path_w);
 		
-		bar.setValue(20);
-		
-		e.extracting(img, w, b, p, a);
-		
-		bar.setValue(75);
-		
+		e.extracting(img, w, b, p, a);		
 		e.postprocessing(w, save, extension);
 		
-		bar.setValue(95);
 	}
 
 	static public double mean_square_error(String bb, String ww){
@@ -106,12 +93,9 @@ public class MainProcess {
 		double w_sum_R = 0;double w_sum_G = 0;double w_sum_B = 0;
 		double b_w_R = 0;double b_w_G = 0;double b_w_B = 0;
 		
-		/*double[][] bY = new double[b.getWidth()][b.getHeight()];
-		double[][] wY = new double[w.getWidth()][w.getHeight()];*/
 		for(int i = 0; i < b.getWidth(); i++){
 			for(int j = 0; j < b.getHeight(); j++){
-				/*bY[i][j] = Image.RGBtoYUV(b.getRGB(i, j))[0];
-				wY[i][j] = Image.RGBtoYUV(w.getRGB(i, j))[0];*/
+				
 					b_sum_R += Math.pow(Image.RGBtoYUV(b.getRGB(i, j))[0],2);
 					b_sum_G += Math.pow(Image.RGBtoYUV(b.getRGB(i, j))[1],2);
 					b_sum_B += Math.pow(Image.RGBtoYUV(b.getRGB(i, j))[2],2);
@@ -126,13 +110,6 @@ public class MainProcess {
 				
 			}
 		}
-		/*double bE = BlockChoiceEntropy.entropy_calc(BlockChoiceEntropy.histogram_count(bY),b.getWidth()*b.getHeight());
-		System.out.println(bE);
-		double wE = BlockChoiceEntropy.entropy_calc(BlockChoiceEntropy.histogram_count(wY),w.getWidth()*w.getHeight());
-		System.out.println(wE);
-		double bwE = joint_entropy_calc(joint_hist_count(bY,wY), b.getWidth()*b.getHeight());
-		System.out.println(bwE);
-		return ((bE + wE) - bwE)/(bE);*/
 		b_sum_R = Math.sqrt(b_sum_R);
 		b_sum_G = Math.sqrt(b_sum_G);
 		b_sum_B = Math.sqrt(b_sum_B);
@@ -144,13 +121,10 @@ public class MainProcess {
 		double NC_R = (b_w_R / (b_sum_R * w_sum_R));
 		double NC_G = (b_w_G / (b_sum_G * w_sum_G));
 		double NC_B = (b_w_B / (b_sum_B * w_sum_B));
-		/*System.out.println(NC_R);
-		System.out.println(NC_G);
-		System.out.println(NC_B);*/
+
 		if (b.getType() == BufferedImage.TYPE_BYTE_GRAY)
 			return NC_R;
 		return ( (NC_R + NC_G + NC_B) / 3 );
-		//return NC_R;
 		}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -174,7 +148,6 @@ public class MainProcess {
 			for(int j = 0; j < p.length; j++){
 				if(p[i][j]!=0){
 					double x = (p[i][j] / (double)size);
-					//System.out.println(p[i][j]);
 					suma -= (x * Math.log(x));
 				}
 			}
