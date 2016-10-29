d5239b880e48d75d4eb89fb8cf8df6d17a222ff1
oneoftheidiots <antonela.mrkalj@gmail.com>
2016-10-29 15:36:49 +0100

    Update BlockChoiceEntropy.java

--- WH_ver2/src/block/BlockChoiceEntropy.java
+++ WH_ver2/src/block/BlockChoiceEntropy.java
@@ -10,30 +10,20 @@ public class BlockChoiceEntropy extends BlockChoice{
 	public ArrayList<Integer> fja_odaberi_blokove(ArrayList<double[][]> niz_mat) {
 		ArrayList <Integer> niz = new ArrayList <Integer>();
 		HashMap <Integer, Double> mapa = new HashMap <Integer, Double>();
-		//ArrayList <Double> entropija = new ArrayList<Double>();
-		//double min = 0;
+		
 		for(int i = 0; i < niz_mat.size(); i++){
 			int[] h_c = histogram_count(niz_mat.get(i));
 			double e = entropy_calc(h_c, (int)Math.pow(niz_mat.get(i).length, 2));
 			mapa.put(i, e);
-			//entropija.add(e);
-			//if( min > e)
-			//	min = e;
+			
 		}
-		//System.out.println(min);
 		Stream<Entry<Integer, Double>> sorted = mapa.entrySet().stream().sorted(Map.Entry.comparingByValue());
 		java.util.Iterator<Entry<Integer, Double>> t = sorted.iterator();
 		
 		while(t.hasNext()){
 			Entry<Integer, Double> combo = t.next();
-			//if(combo.getValue() <= (min - (min*0.5)))
 			niz.add(combo.getKey());
 		}
-//		for(int i = 0; i < niz_mat.size(); i++){
-//			if(entropija.get(i) <= (min -(min*0.5)))
-//				niz.add(i);
-//		}
-		//System.out.println(niz.size());
 		return niz;
 	}
 	
